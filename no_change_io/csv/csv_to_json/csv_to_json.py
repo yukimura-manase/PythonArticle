d8404b8a1b2d63ea452d10aa44fce2309910c131
yukimura-manase <masahiro19951212@gmail.com>
2024-01-09 10:11:28 +0900

    CSV処理・追加

--- no_change_io/csv/csv_to_json/csv_to_json.py
+++ no_change_io/csv/csv_to_json/csv_to_json.py
@@ -3,15 +3,15 @@ import os
 import csv
 import json
 
-csv_file = '業種分類.csv'
+csv_file = 'robotama.csv'
 
 csv_file_path = f'{os.getcwd()}/{csv_file}'
 
-# Read CSV file and convert to JSON
+# CSVファイルを読み込んでJSONに変換する
 with open(csv_file_path, 'r') as csv_file:
     csv_data = csv.DictReader(csv_file)
     json_data = json.dumps([row for row in csv_data], ensure_ascii=False)  # ensure_ascii=False は、日本語をそのまま表示させるため。
 
-# Save JSON data to a file
-with open('industry.json', 'w') as json_file:
+# JSONデータをファイルに保存する
+with open('robotama.json', 'w') as json_file:
     json_file.write(json_data)
